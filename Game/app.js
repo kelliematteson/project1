bce2959c250cae4b047d080ee99c3b5338e23cc1
Kellie Matteson <kelliematteson@yahoo.com>
2020-12-07 14:13:34 -0800

    again

--- Game/app.js
+++ Game/app.js
@@ -1,2 +1,13 @@
-alert('this is working!');
-console.log($);
+//cache dom nodes
+const eachCard = document.getElementsByClassName('card');
+
+$(() => {
+    const pickACard = ()=> {
+        eachCard.addEventListener('click', (event)=> {
+            
+            alert('this works!');
+        })
+    }
+  
+  
+});
